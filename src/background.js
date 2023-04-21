d66612f43625fe909ea58d66ddcb12e5add9db7f
web <a13308263216@gmail.com>
2023-04-22 00:38:01 +0800

    commit

--- src/background.js
+++ src/background.js
@@ -5,17 +5,13 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ['contentScript.js'],
+      });
+    }
+  });
 });
